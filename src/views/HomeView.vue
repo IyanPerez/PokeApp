b763ab11705b86f466dbe0ec032605f8a53b8dcb
Your Name <iyanperezmarcos@gmail.com>
2023-01-09 15:27:03 +0100

    project cleaned and prepared for working

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,15 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import Header from '../components/Header.vue'
+import Footer from '../components/Footer.vue'
+import Cards from '../components/Cards.vue'
+
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <Header />
+    <Cards />
+    <Footer />
+
   </main>
 </template>
