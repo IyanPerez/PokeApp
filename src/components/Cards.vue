83ded3725ae3135d73871775053c76400a827f19
Your Name <iyanperezmarcos@gmail.com>
2023-01-11 15:25:52 +0100

    Cards template structure added

---
+++ src/components/Cards.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    
+}
+</script>
+
+<template>
+    <div id="card">
+        <p>{{ number }} + {{ name }}</p>
+        <img src="" alt="character image">
+    </div>
+    <h1>linea de prueba</h1>
+
+</template>
+
+<style lang="scss" scoped>
+    
+</style>
---
+++ src/views/CardsView.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+</script>
+
+<template>
+    <div id="cards-container">
+        <div
+            class="character"
+            v-for="character in store.poke"
+            :key="character"
+        >
+            <Cards
+             
+            />
+            
+        </div>
+    </div>
+</template>
+
+<style>
+
+</style>
