18c57099ea8b03779156ea82b88f48cfddd9cf6f
Manu-Estrada <mirucuxusticieru@gmail.com>
2023-01-12 15:06:18 +0100

    header and footer finished

---
+++ src/components/Footer.vue
@@ -0,0 +1,66 @@
+<template>
+  <footer class="footer">
+    <div class="footer-container-copyright">
+        <img src="../assets/img/copyright.png">
+        <p><strong>COPYRIGHT</strong></p>
+    </div>
+    <div class="footer-container-social-media">
+      <p><strong>SOCIAL MEDIA</strong></p>
+      <img src="../assets/img/facebook.png">
+      <img src="../assets/img/instagram.png">
+      <img src="../assets/img/twitter.png">
+      <img src="../assets/img/youtube.png">
+    </div>
+  </footer>
+</template>
+
+<style lang="scss">
+.footer {
+  
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+   /* height: auto;*/
+    background: linear-gradient(
+      90deg,
+      red 0%,
+      rgb(215, 232, 226) 99%
+    );
+    /*flex-direction: row;
+    flex-wrap: wrap;*/
+}
+
+.footer img{
+  width: 40px; 
+
+
+}
+
+.footer-container-social-media {
+display: flex;
+gap: 1%;
+margin-right: 2%;
+text-align:end;
+text-shadow:  5px 5px 5px blue;
+}
+
+.footer-container-copyright p strong {
+  text-shadow:  5px 5px 5px blue;
+}
+
+.footer-container-copyright{
+  background: linear-gradient(90deg, red 0%, red 99%);
+}
+
+
+
+.footer-container-social-media img{
+  &:hover {
+    width: 50px;
+}
+}
+.footer-container-social-media strong {
+  margin-right: 10%;
+}
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,41 @@
+<template>
+    <header class="header">
+      <div>
+        <h1>POKÉMON API</h1>
+      </div>
+      <nav>
+        <router-link to="/" class="nav__link" href="">INIT</router-link>
+        <router-link to="/favorites" class="nav__link" href="">SEEKER</router-link>
+        <router-link to="/" class="nav__link" href="">CATEGORIES</router-link>
+      </nav>
+    </header>
+  </template>
+  
+  <style lang="scss">
+  @import "../assets/scss/styles.scss";
+  
+  *{
+    width: 100%;
+  }
+  div{
+    background: linear-gradient(
+      90deg,
+      red 0%,
+      rgb(215, 232, 226) 99%);
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: auto;
+      color: yellow;
+      
+      
+      h1{
+        text-shadow:  10px 10px 10px blue;
+        font-family: $font-text;
+        font-size: 4rem;
+        letter-spacing: 5px;
+        text-align: center;
+ }
+}
+
+
