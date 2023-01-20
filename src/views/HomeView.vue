345412738a97527d7a99d95e84dad47d5de3a13b
Your Name <iyanperezmarcos@gmail.com>
2023-01-20 09:43:10 +0100

    cards structure

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,17 @@
 <script setup>
-import Header from '../components/Header.vue'
-import Footer from '../components/Footer.vue'
+import { ref } from "vue";
 import Cards from '../components/Cards.vue'
+
+
+async function fetch() {
+  const pokemons = ref([])
+  const response = await fetch("https://fakestoreapi.com/products");
+  const data = response.json();
+  this.pokemons = data;
+}
+
+fetch();
+console.log(data);
 
 </script>
 
